a1a331240d60a1a631f1b748fcad5681063b14ae
Rostyslav Rachynskiy <r.rostik2000@gmail.com>
2020-04-26 04:13:49 +0300

    Changed url to route

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(morgan('dev'));
 app.use(dbPool);
-app.use('/profile', profileRoutes);
+app.use(profileRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
